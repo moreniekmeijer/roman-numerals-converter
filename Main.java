ccde5e300b96077cc27dd1ef26e26f48df975746
moreniekmeijer <niekjmeijer@gmail.com>
2024-12-07 11:51:26 +0100

    first commit

---
+++ Main.java
@@ -0,0 +1,49 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // initialiseer hier een Romeins getal
+        String roman = "MMXXIV";
+
+        // Aanroep van de methode
+        romanToInt(roman);
+
+    }
+
+    public static void romanToInt(String romanNumber) {
+
+        // HashMap om de Romeinse karakters te linken aan een numerieke waarde (Integer):
+        HashMap<Character, Integer> romanValues = new HashMap<>();
+        romanValues.put('I', 1);
+        romanValues.put('V', 5);
+        romanValues.put('X', 10);
+        romanValues.put('L', 50);
+        romanValues.put('C', 100);
+        romanValues.put('D', 500);
+        romanValues.put('M', 1000);
+
+        // Initialisatie van de totale waarde en de 'vorige' waarde.
+        int totalValue = 0;
+        int previousValue = 0;
+
+        // Omgekeerde for-loop zodat ik de Romeinse karakters ook eventueel kan aftrekken van het karakter dat erna komt.
+        for (int i = romanNumber.length() - 1; i >= 0; i--) {
+
+            // initialiseert characters op basis van de index uit de loop, daarna linkt deze de correcte Integer uit de HashMap
+            char currentChar = romanNumber.toUpperCase().charAt(i);
+            int currentValue = romanValues.get(currentChar);
+
+            // Deze if-else checkt of het nieuwe karakter uit de index groter of kleiner is dan de vorige,
+            // zodat het opgeteld of afgetrokken kan worden van de totale waarde.
+            if (currentValue >= previousValue) {
+                totalValue += currentValue;
+                previousValue = currentValue;
+            } else {
+                totalValue -= currentValue;
+                previousValue = currentValue;
+            }
+        }
+        System.out.println(totalValue);
+    }
+}
